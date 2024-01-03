ce3c009e22070a9236a924c48368e380af49fcd1
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2024-01-03 15:51:09 -0500

    python solver works

--- py/solver.py
+++ py/solver.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import string
-from collections import Counter
+from collections import Counter, defaultdict
 
 class PositionalRequirements:
     def __init__(self) -> None:
@@ -14,7 +14,8 @@ class PositionalRequirements:
 class Constraints:
     def __init__(self, length: int) -> None:
         self.letter_counts = {letter: [0, 5] for letter in string.ascii_uppercase}
-        self.positional_requirements = [PositionalRequirements()] * length
+        self.positional_requirements = [PositionalRequirements() for _ in range(length)]
+        self.length = length
 
     def __str__(self):
         return f"Constraints: {self.positional_requirements}; {self.letter_counts}"
@@ -22,28 +23,32 @@ class Constraints:
 
 class Word:
     def __init__(self, word: str) -> None:
-        self.positional = word.split()
-        self.letter_counts = {letter: count for letter, count in Counter(word).items()}
+        self.positional = list(word)
+        self.letter_counts = defaultdict(int, {letter: count for letter, count in Counter(word).items()})
+
     def __str__(self):
-        return "".join(self.positional)
+        return f"{''.join(self.positional)}"
 
     def is_valid(self, constraints: Constraints) -> bool:
-        try:
-            for i, position in enumerate(self.positional):
-                if constraints.positional_requirements[i].required and constraints.positional_requirements[i].required != position:
-                        return False
-                else:
-                    if position in constraints.positional_requirements[i].banned:
-                        return False
+        if constraints.length != len(self.positional):
+            return False
 
-            for letter in self.letter_counts:
-                if constraints.letter_counts[letter][0] > self.letter_counts[letter] or constraints.letter_counts[letter][1] < self.letter_counts[letter]:
+        for i, position in enumerate(self.positional):
+            if constraints.positional_requirements[i].required and constraints.positional_requirements[i].required != position:
+                return False
+            else:
+                if position in constraints.positional_requirements[i].banned:
                     return False
 
-            return True
+        for letter in constraints.letter_counts:
+            if constraints.letter_counts[letter][0] > self.letter_counts[letter]:
+                return False
 
-        except KeyError:
-            return False
+            if constraints.letter_counts[letter][1] < self.letter_counts[letter]:
+                return False
+
+        return True
+
 
 
 
@@ -74,8 +79,33 @@ class WordBank:
 def main():
     bank = WordBank.load_from_file()
     constraints = Constraints(5)
-    # constraints.letter_counts['Y'] = [2, 2]
-    constraints.positional_requirements[2].required = 'A'
+
+    constraints.letter_counts['A'] = [0, 0]
+    constraints.letter_counts['L'] = [1, 5]
+    constraints.letter_counts['I'] = [1, 5]
+    constraints.letter_counts['E'] = [0, 0]
+    constraints.letter_counts['N'] = [0, 0]
+    constraints.letter_counts['O'] = [0, 0]
+    constraints.letter_counts['S'] = [0, 0]
+    constraints.letter_counts['P'] = [0, 0]
+    constraints.letter_counts['T'] = [1, 5]
+    constraints.letter_counts['R'] = [1, 5]
+
+
+    constraints.positional_requirements[1].banned.append('L')
+    constraints.positional_requirements[2].required = 'I'
+
+    constraints.positional_requirements[3].banned.append('I')
+    constraints.positional_requirements[4].required = 'L'
+
+    constraints.positional_requirements[0].required = 'T'
+    constraints.positional_requirements[1].banned.append('R')
+
+    words = [Word('BOOKS'), Word('BROKE'), Word('BRAKE'), Word('CROOK'), Word('OAAAO')]
+
+
+
+
     valid_words = bank.find_valid_words(constraints)
     for word in valid_words:
         print(word)
